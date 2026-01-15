b80fdf695caecf8b93594d4e90c1b6ca3ddec697
Samantha <samantha@uppointinc.com>
2026-01-14 16:36:40 -0800

    fixed db mapping

--- pkg/models/url.go
+++ pkg/models/url.go
@@ -3,8 +3,8 @@ package models
 import "time"
 
 type Url struct {
-	URL_ID		uint		`gorm:"primaryKey"`
-	CreatedAt	time.Time
-	LongURL 	string
-	ShortURL 	string
+	URL_ID		uint		`gorm:"primaryKey;column:url_id"`
+	CreatedAt	time.Time	`gorm:"column:created_at"`
+	LongURL 	string		`gorm:"column:long_url"`
+	ShortURL 	string		`gorm:"column:short_url"`
 }
